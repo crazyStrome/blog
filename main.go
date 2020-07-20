4b39774e6cf617125accb5cc50d7499c58abb10f
hsp <1415441419@qq.com>
2020-07-20 20:28:20 +0800

    添加三个网页

--- main.go
+++ main.go
@@ -2,28 +2,49 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.Use(logger())
-	router.LoadHTMLFiles("./statics/index.html")
-	router.StaticFile("/css", "./css")
-	router.StaticFile("/img", "./img")
-	router.StaticFile("/scss", "./scss")
-	router.StaticFile("/vendor", "./vendor")
-	router.GET("/", func(c *gin.Context) {
-		// c.HTML(http.StatusOK, "index.html", "sss")
-		c.String(200, "宝贝😍，嘻嘻~")
-	})
+	// router.LoadHTMLFiles("./statics/index.html")
+	router.LoadHTMLGlob("./view/*")
+	router.Static("/css", "./statics/css")
+	router.Static("/img", "./statics/img")
+	router.Static("/scss", "./statics/scss")
+	router.Static("/vendor", "./statics/vendor")
+	router.Static("/js", "./statics/js")
+	router.Static("/fonts", "./statics/fonts")
+	router.Static("/images", "./statics/images")
+	router.GET("/", indexPage)
+	router.GET("/login", loginPage)
+	router.POST("/login", loginPage)
+	router.POST("/home", homePage)
+	router.GET("/lost", lostPage)
 	router.Run()
 }
-func logger() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		fmt.Println("1111")
-		c.Next()
-		fmt.Println("2222")
-	}
+func indexPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", "/login")
+}
+func loginPage(c *gin.Context) {
+	var email, _ = c.GetPostForm("email")
+	fmt.Println(email)
+	c.HTML(http.StatusOK, "login.html", gin.H{
+		"email": email,
+		"url":   "/home",
+		"lost":  "/lost",
+	})
+}
+func homePage(c *gin.Context) {
+	// var email, _ = c.GetPostForm("email")
+	// var passwd, _ = c.GetPostForm("password")
+	c.HTML(200, "home.html", gin.H{
+		"name": "宝贝",
+		"home": "/home",
+	})
+}
+func lostPage(c *gin.Context) {
+
 }
